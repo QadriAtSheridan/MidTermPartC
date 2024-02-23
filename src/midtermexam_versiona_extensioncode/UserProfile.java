06e24958fe6d3000d045fda13341e8db25940dad
QadriAtSheridan <148021209+QadriAtSheridan@users.noreply.github.com>
2024-02-23 17:39:05 -0500

    Update UserProfile.java comment for fetch+merge

--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -12,6 +12,7 @@ package midtermexam_versiona_extensioncode;
  * Students are reminded to add themselves as modifiers when editing the
  * code.
  * @author dancye, 2019
+ *Ibrahim Qadri's change to the new branch for a fetch and merge 
  */
 public class UserProfile 
 {
